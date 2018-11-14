1d6f697747a3e0e4a984b158c7c7483358002de7
derozon <15099661014@163.com>
2018-11-14 16:45:00 +0800

    beginning

---
+++ src/main/java/Car.java
@@ -0,0 +1,33 @@
+public class Car {
+
+
+    private int id;
+
+    private String carName;
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", carName='" + carName + '\'' +
+                '}';
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+}
